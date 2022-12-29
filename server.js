7581507cd49b52ca53f1b17bdb85bb97e8fd8f30
Jill Arnold <jillarnold2913@gmail.com>
2022-12-29 11:24:48 -0500

    Example to pass data between middleware functions

--- server.js
+++ server.js
@@ -1,33 +1,20 @@
 const express = require("express");
 const app = express();
 
-const first = (req, res, next) => {
-  console.log("First");
+//TO PASS DATA BETWEEN MIDDLEWARE FUNCTIONS
+//Remember, with each "request/response cycle", 
+//there is only one request object and one response 
+//object, so we can use those to pass data between middleware functions.
+app.use((req, res, next) => {
+  req.requestTime = new Date().toString();
   next();
-};
-
-const second = (req, res, next) => {
-  console.log("Second");
-  next();
-};
-
-const third = (req, res, next) => {
-  console.log("Third");
-  next();
-};
-
-app.use(first);
-app.use(second);
-app.use(third);
-
-app.get("/", function (req, res) {
-  res.send("Hello World!");
 });
-//BROWSER PRINTS HELLO WORLD, TERMINAL CONSOLE LOGS FIRST, SECOND, THIRD
-//First
-//Second
-//Third
+
+app.get("/", (req, res) => {
+  const responseText = "Requested at: " + req.requestTime + ". Like a boss! ";
+  res.send(responseText);
+});
+//Requested at: Thu Dec 29 2022 11:24:09 GMT-0500 (Eastern Standard Time). Like a boss!
 
 app.listen(8000);
-
 
